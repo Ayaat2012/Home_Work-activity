f401201cfd74d841f3afd24e4510f7f424d7d895
Ayaat2012 <iffatrajshahi@gmail.com>
2025-12-05 20:09:00 +0600

    initial commit

---
+++ Project-1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlite3
+
+database = 'database.sqlite'
+
+conn = sqlite3.connect(database)
+print("Opened data successfully.")
+
+
+#Read sql query for getting all the tables of database into dataform
+table = pd.read_sql("""SELECT * FROM sqlite_master
+                    WHERE type = 'table';""", conn)
+
+print(table)
